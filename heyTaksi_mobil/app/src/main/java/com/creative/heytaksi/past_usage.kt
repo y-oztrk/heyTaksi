76d15f4c6abc5415ad9c521d7d7f1bd104042d5f
DESKTOP-U2N87OI\Yigitcan <yigitcanozturk@ogr.bandirma.edu.tr>
2021-02-05 22:19:03 +0300

    Proje Github'a Yüklendi

---
+++ heyTaksi_mobil/app/src/main/java/com/creative/heytaksi/past_usage.kt
@@ -0,0 +1,11 @@
+package com.creative.heytaksi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class past_usage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_past_usage)
+    }
+}
